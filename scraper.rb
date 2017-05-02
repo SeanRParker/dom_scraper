a2f20a76e8a0fbad58567c436fe0b4f0f6b4b7b0
Shaws <seanrichardparker@gmail.com>
2017-05-02 08:21:11 -0400

    adding global variables to replace instance vars

--- scraper.rb
+++ scraper.rb
@@ -2,6 +2,14 @@ require 'Mechanize'
 require 'Nokogiri'
 require 'Pry'
 
+# Use global variables - requires testing
+username = ""
+user_pass = ""
+amount_due = ""
+bill_amount = ""
+start_date = ""
+end_date = ""
+
 mech = Mechanize.new
 
 page = mech.get('https://mya.dom.com/')
@@ -16,9 +24,9 @@ puts "Please input your login info for Dominion (I'm sure this is typically stor
 # Login
 def login
 puts "username:"
-@username = gets.chomp
+username = gets.chomp
 puts "password:"
-@user_pass = gets.chomp
+user_pass = gets.chomp
 end
 login
 
@@ -26,16 +34,16 @@ puts "Logging in... one sec.."
 
 # Fill out Sign in for with Mechanize, hit submit button and change page var
 form = page.forms.first
-form['user'] = @username
-form['password'] = @user_pass
+form['user'] = username
+form['password'] = user_pass
 page = form.submit
 
-@h1 = page.css("h1").text
+h1 = page.css("h1").text
 
-if @h1 == "Sign In"
+if h1 == "Sign In"
   puts "Check your username and password and try again"
   login
-elsif @h1 == "My Account Overview"
+elsif h1 == "My Account Overview"
   puts "Successfully logged in"
 else
   puts "Something went wrong"
@@ -43,7 +51,7 @@ end
 
 # View Past Usage page
 due = page.css("table#billingAndPaymentsTable tbody tr[2] td[12]").inner_html()
-@amount_due = due.to_i
+amount_due = due.to_i
 
 # Switch to Analyze Energy Usage page
 link = page.link_with(text: 'Analyze Energy Usage')
@@ -52,15 +60,15 @@ page = link.click
 # Test to make sure user got in, else send back to login.
 usage = Nokogiri::HTML(page.body)
 
-@bill_amount = usage.css("table#billHistoryTable tbody tr[2] td[3]").text()
-@start_date = usage.css("table#paymentsTable tbody tr[3] td[1]").text()
-@end_date = usage.css("table#paymentsTable tbody tr[2] td[1]").text()
+bill_amount = usage.css("table#billHistoryTable tbody tr[2] td[3]").text()
+start_date = usage.css("table#paymentsTable tbody tr[3] td[1]").text()
+end_date = usage.css("table#paymentsTable tbody tr[2] td[1]").text()
 
 def calculate_bill
-  if @amount_due == 0
+  if amount_due == 0
     puts "You don't owe anything!"
   else
-    puts "You owe: #{@bill_amount.to_i}"
+    puts "You owe: #{bill_amount.to_i}"
   end
 end
 calculate_bill
@@ -72,12 +80,12 @@ kwh = usage.css('table#paymentsTable tbody tr[2] td[3]').text()
 # usage (kWh), bill amount ($), service start date, service end date
 bill_details = {}
 bill_details[:kwh] = kwh
-bill_details[:bill_amount] = @bill_amount
-bill_details[:start_date] = @start_date
-bill_details[:end_date] = @end_date
+bill_details[:bill_amount] = bill_amount
+bill_details[:start_date] = start_date
+bill_details[:end_date] = end_date
 
 # TODO
 # Create method that calls in different tables with various cells, rows, and table IDs as parameters into Nokogiri
-# Use global variables instead of instance vars
 # Look into classes for this project
 # Call in ENV vars (credentials)
+# Nokogiri not returning second table of Analyze Energy User page. Convert app to Watir gem to see if that will do it.
